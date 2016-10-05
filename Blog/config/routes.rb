5477e12bc38cb30128aa8072b230abd0248752e1
giathanhphat <gtphat94@gmail.com>
2016-10-05 21:04:18 +0700

    Blog init.

---
+++ Blog/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+	#---create page about and contact
+  get 'about' => 'page#about', as: :about
+
+ get 'contact' => 'page#contact', as: :contact
+# page default
+	root to: 'posts#index'
+  resources :posts
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
