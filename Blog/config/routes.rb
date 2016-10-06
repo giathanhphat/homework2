efff95fc31b22a196c0676aec535cac94cce0033
giathanhphat <gtphat94@gmail.com>
2016-10-07 01:26:43 +0700

    Them tinh nang Comment.

--- Blog/config/routes.rb
+++ Blog/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'comments/create'
+
+  get 'comments/destroy'
+
 	#---create page about and contact
   get 'about' => 'page#about', as: :about
 
@@ -8,6 +12,8 @@ Rails.application.routes.draw do
   get 'mypost' => 'page#mypost', as: :mypost
 # page default
 	root to: 'posts#index'
-  resources :posts
+  resources :posts do
+  	resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- Blog/db/schema.rb
+++ Blog/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004070821) do
+ActiveRecord::Schema.define(version: 20161006142840) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "post_id"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
